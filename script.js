8ec448f80e3c91b01725f5184c53b374cd8966da
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-06-20 23:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let n = 0;
+	for(let i=0; i<library.length; i++) {
+		if(library[i].readingStatus)
+			n++;
+	}
+	return n;
 };
 
 // Do not change the code below
